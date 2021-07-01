327d3414b7eea11a22bfaccbc41018aa78fb11e2
Bagja <bagja.basith0612@students.amikom.ac.id>
2021-07-02 02:25:34 +0700

    beresin final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-let labels1 = ['Khudd am', 'LI','Athfal','Nashirat','Anshar','Abna','Banat','Simpatisan'];
+let labels1 = ['Khuddam', 'LI','Athfal','Nashirat','Anshar','Abna','Banat','Simpatisan'];
 
-let data1 = [254,433,1397,1534,1,75,57,21];
+let data1 = [325,488,1664,1803,10,92,71,39];
 let colors1 = ['#49A9EA', '#36CAAB','rgb(158, 151, 151)','rgb(56, 52, 52)','#7beec7','rgb(255, 174, 0)','#437423','rgb(255, 0, 0)']
 
 let myChart = document.getElementById("myChart").getContext('2d');
